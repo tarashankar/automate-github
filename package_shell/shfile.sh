4b700888c254759a6b1716be53dc1d75ff18a6ae
tarashankar <gtataracity@gmail.com>
2016-06-30 20:01:10 +0530

    Update shfile.sh

--- package_shell/shfile.sh
+++ package_shell/shfile.sh
@@ -7,6 +7,6 @@ cd created-by-ui
 touch readme2.txt
 git add readme2.txt
 git commit -m "readme2.txt added by script"
-git push https://tarashankar:MARIOismy123!@github.com/tarashankar/created-by-ui.git
+git push https://username:password@github.com/yusername/created-by-ui.git
 
 # git pull https://username:password@mygithost.com/my/repository
